31a587c2181ad6850e2fea89731335b8e7d9bf41
AshanSandeep06 <ashansandeep06@gmail.com>
2022-09-27 19:53:25 +0530

    Content Updated in Web POS System

--- assignments/css/Frameworks/POS/assets/js/script.js
+++ assignments/css/Frameworks/POS/assets/js/script.js
@@ -1,8 +1,25 @@
-$("#btnSaveCustomer").click(function (){
-    let customerID = $("#txtCustomerID").val();
-    let customerName = $("#txtCustomerName").val();
-    let cusAddress = $("#txtAddress").val();
-    let cusSalary = $("#txtSalary").val();
+$("#btnSaveCustomer").click(function () {
+    let customerID = "";
+    let customerName = "";
+    let cusAddress = "";
+    let cusSalary = "";
+
+    customerID = $("#txtCustomerID").val();
+    customerName = $("#txtCustomerName").val();
+    cusAddress = $("#txtAddress").val();
+    cusSalary = $("#txtSalary").val();
+
+    /* Check if the customer fields have typed values */
+    if (customerID.length !== 0 && customerName.length !== 0 && cusAddress.length !== 0 && cusSalary.length !== 0) {
+
+        /* Customer Object */
+        var customerObject = {
+            id: customerID,
+            name: customerName,
+            address: cusAddress,
+            salary: cusSalary
+        }
 
 
+    }
 });
