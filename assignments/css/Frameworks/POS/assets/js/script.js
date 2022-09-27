83bb7ae6c6bba58bd505ff514f39318a8b8831e8
AshanSandeep06 <ashansandeep06@gmail.com>
2022-09-27 19:41:01 +0530

    Introduced js variables for getting input fields' values in Customer Form

--- assignments/css/Frameworks/POS/assets/js/script.js
+++ assignments/css/Frameworks/POS/assets/js/script.js
@@ -1,3 +1,8 @@
 $("#btnSaveCustomer").click(function (){
+    let customerID = $("#txtCustomerID").val();
+    let customerName = $("#txtCustomerName").val();
+    let cusAddress = $("#txtAddress").val();
+    let cusSalary = $("#txtSalary").val();
+
 
 });
