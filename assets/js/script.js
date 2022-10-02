edbcce84f8e11ca0331d09f446b913a6ad0cfdbe
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-02 14:29:06 +0530

    Added a click function for View More btn in Projects Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 /* ------------------------------------ My Portfolio js file ------------------------------------------------ */
 
+$("#btnProjectsViewMore").on("click", function () {
+    $("#btnProjectsViewMore>a").attr("target", "_blank");
+    $("#btnProjectsViewMore>a").attr("href", "https://github.com/AshanSandeep06?tab=repositories");
+});
