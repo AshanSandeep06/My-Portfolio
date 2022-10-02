de7f4da73cd809dbff8afe1337961c29de6d4660
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-02 14:41:13 +0530

    Content Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,17 @@ $("#btnProjectsViewMore").on("click", function () {
     $("#btnProjectsViewMore>a").attr("href", "https://github.com/AshanSandeep06?tab=repositories");
 });
 
-$('#btnPOSProject').on('click',function (){
+$('#btnPOSProject').on('click', function () {
     $("#btnPOSProject>a").attr("target", "_blank");
     $("#btnPOSProject>a").attr("href", "https://github.com/AshanSandeep06/Restaurant_Management-POS_System");
 });
 
-$('#btnHostelProject').on('click',function (){
+$('#btnHostelProject').on('click', function () {
     $("#btnHostelProject>a").attr("target", "_blank");
     $("#btnHostelProject>a").attr("href", "https://github.com/AshanSandeep06/Hostel_Management_System-Hibernate-CourseWork");
 });
 
-$('#btnHotelReservationProject').on('click',function (){
+$('#btnHotelReservationProject').on('click', function () {
     $("#btnHotelReservationProject>a").attr("target", "_blank");
     $("#btnHotelReservationProject>a").attr("href", "https://github.com/AshanSandeep06/Hotel_Reservation-System");
 });
