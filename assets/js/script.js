5a6733cfad1058ab0cd25f5f837bc06f1e412794
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-02 14:32:30 +0530

    Content Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ $("#btnProjectsViewMore").on("click", function () {
     $("#btnProjectsViewMore>a").attr("target", "_blank");
     $("#btnProjectsViewMore>a").attr("href", "https://github.com/AshanSandeep06?tab=repositories");
 });
+
+$('#btnPOSProject').on('click',function (){
+    $("#btnPOSProject>a").attr("target", "_blank");
+    $("#btnPOSProject>a").attr("href", "https://github.com/AshanSandeep06?tab=repositories");
+});
