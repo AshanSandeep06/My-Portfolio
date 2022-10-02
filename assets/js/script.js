9d170bd49d8c847047ae63a1b3885f9ea360ab2f
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-02 14:38:37 +0530

    Set click functions for Project buttons in Projects Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,15 @@ $("#btnProjectsViewMore").on("click", function () {
 
 $('#btnPOSProject').on('click',function (){
     $("#btnPOSProject>a").attr("target", "_blank");
-    $("#btnPOSProject>a").attr("href", "https://github.com/AshanSandeep06?tab=repositories");
+    $("#btnPOSProject>a").attr("href", "https://github.com/AshanSandeep06/Restaurant_Management-POS_System");
+});
+
+$('#btnHostelProject').on('click',function (){
+    $("#btnHostelProject>a").attr("target", "_blank");
+    $("#btnHostelProject>a").attr("href", "https://github.com/AshanSandeep06/Hostel_Management_System-Hibernate-CourseWork");
+});
+
+$('#btnHotelReservationProject').on('click',function (){
+    $("#btnHotelReservationProject>a").attr("target", "_blank");
+    $("#btnHotelReservationProject>a").attr("href", "https://github.com/AshanSandeep06/Hotel_Reservation-System");
 });
