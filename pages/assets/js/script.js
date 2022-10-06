1e6c91f1e6ebb8da34b1716d5e58796a4aa6a677
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-06 15:59:50 +0530

    Linked the Projects to Projects Page using jQuery click functions

--- pages/assets/js/script.js
+++ pages/assets/js/script.js
@@ -1,6 +1,34 @@
 /* ----------------------- js file for Projects and Assignments Page ----------------------- */
 
 /* ------------------------ Projects Page -------------------- */
+$('#btnProject1').on('click', function () {
+    $("#btnProject1>a").attr("target", "_blank");
+    $("#btnProject1>a").attr("href", "https://github.com/AshanSandeep06/Restaurant_Management-POS_System");
+});
 
+$('#btnProject2').on('click', function () {
+    $("#btnProject2>a").attr("target", "_blank");
+    $("#btnProject2>a").attr("href", "https://github.com/AshanSandeep06/Hostel_Management_System-Hibernate-CourseWork");
+});
+
+$('#btnProject3').on('click', function () {
+    $("#btnProject3>a").attr("target", "_blank");
+    $("#btnProject3>a").attr("href", "https://github.com/AshanSandeep06/Hotel_Reservation-System");
+});
+
+$('#btnProject4').on('click', function () {
+    $("#btnProject4>a").attr("target", "_blank");
+    $("#btnProject4>a").attr("href", "https://github.com/AshanSandeep06/Supermarket_System-Layered_Architecture.git");
+});
+
+$('#btnProject5').on('click', function () {
+    $("#btnProject5>a").attr("target", "_blank");
+    $("#btnProject5>a").attr("href", "https://github.com/AshanSandeep06/Vehicle_Parking-System.git");
+});
+
+$('#btnProject6').on('click', function () {
+    $("#btnProject6>a").attr("target", "_blank");
+    $("#btnProject6>a").attr("href", "https://github.com/AshanSandeep06/My_Chat_Application-INP_Coursework.git");
+});
 
 /* ------------------------ Assignments Page -------------------- */
