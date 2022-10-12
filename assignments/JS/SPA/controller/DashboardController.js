706a71696eb9fb52b1c89a9a2246e1306ae9276e
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-12 21:19:51 +0530

    DashBoardController js file was updated and linked this file to index file

---
+++ assignments/JS/SPA/controller/DashboardController.js
@@ -0,0 +1,39 @@
+/*This will only run once, When the DOM is ready for JavaScript code to execute.*/
+$(function () {
+    $('#dashBoardSection').css("display", "block");
+    $('#customerSection').css("display", "none");
+    $('#itemSection').css("display", "none");
+    $('#placeOrderSection').css("display", "none");
+});
+
+/*When the Home button clicked*/
+$('#homeBtn').click(function () {
+    $('#dashBoardSection').css("display", "block");
+    $('#customerSection').css("display", "none");
+    $('#itemSection').css("display", "none");
+    $('#placeOrderSection').css("display", "none");
+});
+
+/*When the Customer button clicked*/
+$('#customerBtn').click(function () {
+    $('#dashBoardSection').css("display", "none");
+    $('#customerSection').css("display", "block");
+    $('#itemSection').css("display", "none");
+    $('#placeOrderSection').css("display", "none");
+});
+
+/*When the Item button clicked*/
+$('#itemBtn').click(function () {
+    $('#dashBoardSection').css("display", "none");
+    $('#customerSection').css("display", "none");
+    $('#itemSection').css("display", "block");
+    $('#placeOrderSection').css("display", "none");
+});
+
+/*When the Place Order button clicked*/
+$('#placeOrderBtn').click(function () {
+    $('#dashBoardSection').css("display", "none");
+    $('#customerSection').css("display", "none");
+    $('#itemSection').css("display", "none");
+    $('#placeOrderSection').css("display", "block");
+});
