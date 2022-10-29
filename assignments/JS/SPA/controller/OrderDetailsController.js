790a1e394881336f027c55c8c836d7437896ae54
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-29 11:58:26 +0530

    Introduced validate event for search Order textField in SPA-Pos

--- assignments/JS/SPA/controller/OrderDetailsController.js
+++ assignments/JS/SPA/controller/OrderDetailsController.js
@@ -1,1 +1,18 @@
 /* ----------------------------------- Order Details Form ------------------------------------ */
+
+// Reg Ex for Order ID
+const orderIdPattern = /^(OID-)[0-9]{3}$/;
+
+// Initially, Search Order button was disabled
+$('#btnSearchOrder').attr('disabled', true);
+
+// Validate txtSearchOrder textField
+$('#txtSearchOrder').on('keyup', function (event) {
+    if ($('#txtSearchOrder').val().length !== 0) {
+        if (orderIdPattern.test($('#txtSearchOrder').val())) {
+            $('#btnSearchOrder').attr('disabled', false);
+            return;
+        }
+    }
+    $('#btnSearchOrder').attr('disabled', true);
+});
