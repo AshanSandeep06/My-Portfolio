6e985923321ad36e523415992ab30058bb5f1938
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-29 12:49:26 +0530

    Refactored Search Order function in OrderDetailsController of SPA-Pos

--- assignments/JS/SPA/controller/OrderDetailsController.js
+++ assignments/JS/SPA/controller/OrderDetailsController.js
@@ -28,10 +28,38 @@ $('#txtSearchOrder').on('keyup', function () {
     }
 });
 
-$('#btnSearchOrder').click(function () {
+function setOrderData(order) {
+    $('#od_txtOrderID').val(order.orderId);
+    $('#od_txtOrderDate').val(order.cusId);
+    $('#od_txtCustomerID').val(order.orderDate);
+    $('#od_txtTotalCost').val(order.totalCost);
+    $('#od_txtDiscount').val(order.discount);
+}
 
+function searchOrder() {
+    for (let order of orders) {
+        if ($('#txtSearchOrder').val() === order.orderId) {
+            setOrderData(order);
+            return true;
+        }
+    }
+    return false;
+}
+
+function errorAlert() {
+    if (searchOrder() === false) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'There is no Order exists for this Order ID!'
+        })
+    }
+}
+
+$('#btnSearchOrder').click(function () {
+    errorAlert();
 });
 
 $('#txtSearchOrder').on('click', function () {
-
+    errorAlert();
 });
