859dfcf02b918a4b5be063cd6120d7be49df6003
AshanSandeep06 <ashansandeep06@gmail.com>
2022-10-29 12:14:50 +0530

    Content Updated in SPA-POS

--- assignments/JS/SPA/controller/OrderDetailsController.js
+++ assignments/JS/SPA/controller/OrderDetailsController.js
@@ -6,13 +6,24 @@ const orderIdPattern = /^(OID-)[0-9]{3}$/;
 // Initially, Search Order button was disabled
 $('#btnSearchOrder').attr('disabled', true);
 
+// Spans for validations will display: hidden, until that related textField's values are invalid
+$('#searchOrderSpan').css('display', "none");
+
 // Validate txtSearchOrder textField
 $('#txtSearchOrder').on('keyup', function (event) {
     if ($('#txtSearchOrder').val().length !== 0) {
         if (orderIdPattern.test($('#txtSearchOrder').val())) {
             $('#btnSearchOrder').attr('disabled', false);
-            return;
+            $('#searchOrderSpan').css('display', 'none');
+            $('#txtSearchOrder').css("border", "1px solid rgb(206, 212, 218)");
+        } else {
+            $('#btnSearchOrder').attr('disabled', true);
+            $('#searchOrderSpan').css('display', 'block');
+            $('#txtSearchOrder').css("border", "2px solid red");
         }
+    } else {
+        $('#btnSearchOrder').attr('disabled', true);
+        $('#searchOrderSpan').css('display', 'none');
+        $('#txtSearchOrder').css("border", "1px solid rgb(206, 212, 218)");
     }
-    $('#btnSearchOrder').attr('disabled', true);
 });
